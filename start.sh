51063509ba708221bc590b89a0176e3243e33d89
eric-zadara <eric@zadarastorage.com>
2022-04-06 14:48:58 -0700

    docker: only use geoDB if file is larger than 0

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ ARGS=()
 if [[ -n "${MAXMIND_API}" ]]; then
 	for db in 'GeoLite2-City' 'GeoLite2-Country' 'GeoLite2-ASN'; do
 		curl -fsSL -m 30 "https://download.maxmind.com/app/geoip_download?edition_id=${db}&license_key=${MAXMIND_API}&suffix=tar.gz" | tar --wildcards --strip-components=1 -xzf - '*.mmdb'
-		if [[ -e "${db}.mmdb" ]]; then
+		if [[ -s "${db}.mmdb" ]]; then
 			case "${db}" in
 				"GeoLite2-City")
 					ARGS+=( "-c" "${db}.mmdb" )
